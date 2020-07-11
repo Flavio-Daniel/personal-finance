b9ede12c8e0d9f78126c519d8b753a7afad033d6
Flavio-Daniel <flavio.alves@nubank.com.br>
2020-07-11 14:34:39 -0300

    add cheshire lib for json use

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
+                 [cheshire "5.8.1"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
                  [clj-http "3.9.1"]]
