96c9fe217cafd6245579c42fc3e812baa0c09c3e
Flavio-Daniel <flavio.alves@nubank.com.br>
2020-07-11 17:00:17 -0300

    create new test using post

--- test/acceptance/personal_finance/balance_acceptance_test.clj
+++ test/acceptance/personal_finance/balance_acceptance_test.clj
@@ -3,11 +3,20 @@
     [midje.sweet :refer :all]
     [personal-finance.handler :refer [app]]
     [personal-finance.auxiliar :refer :all]
-    [cheshire.core :as json]))
+    [cheshire.core :as json]
+    [clj-http.client :as http]))
 
 (against-background
   [(before :facts (start-server port-default))
    (after :facts (stop-server))]
+
   (fact "The starting balance is 0" :acceptance
         ;;; should return a json format
-        (json/parse-string (content "/balance") true) => {:balance 0}))
+        (json/parse-string (content "/balance") true) => {:balance 0})
+
+  (fact "The balance is 10 after a revenue transaction of 10" :acceptance
+
+        (http/post (route-to "/transactions")
+                   {:body (json/generate-string {:valor 10 :tipo "receita"})})
+
+        (json/parse-string (content "/balance") true) => {:balance 10}))
