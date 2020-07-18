7ec26a3637129ce891cda19ea9fa55ee848bdf96
Flavio-Daniel <flavio.alves@nubank.com.br>
2020-07-18 15:51:14 -0300

    create acceptance tests for filters

---
+++ test/acceptance/personal_finance/filters_acceptance_test.clj
@@ -0,0 +1,49 @@
+(ns personal-finance.filters-acceptance-test
+  (:require [midje.sweet :refer :all]
+            [cheshire.core :as json]
+            [personal-finance.auxiliar :refer :all]
+            [clj-http.client :as http]
+            [personal-finance.db :as db]))
+
+(def random-transactions
+  '({:value 7.0M :type "expense"}
+    {:value 88.0M :type "expense"}
+    {:value 106.0M :type "expense"}
+    {:value 8000.0M :type "revenue"}))
+
+(against-background
+  [(before :facts [(start-server port-default)
+                   (db/clean)])
+   (after :facts (stop-server))]
+
+  (fact "There is not any revenue" :acceptance
+        (json/parse-string (content "/revenues") true)
+        => {:transactions '()})
+
+  (fact "There is not any expense" :acceptance
+        (json/parse-string (content "/expenses") true)
+        => {:transactions '()})
+
+  (fact "There is not any transaction" :acceptance
+        (json/parse-string (content "/transactions") true)
+        => {:transactions '()})
+
+  (against-background
+    [(before :facts [(doseq [transaction random-transactions]
+                       (db/register transaction))])
+     (after :facts (db/clean))]
+
+    (fact "There are 3 expenses" :acceptance
+          (count (:transactions (json/parse-string (content "/expenses") true)))
+          => 3)
+
+    (fact "There is 1 revenue" :acceptance
+          (count (:transactions (json/parse-string (content "/revenues") true)))
+          => 1)
+
+    (fact "There are 4 transactions" :acceptance
+          (count (:transactions (json/parse-string (content "/transactions") true)))
+          => 4)
+
+    ))
+
