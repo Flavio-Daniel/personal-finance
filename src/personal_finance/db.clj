28c07c5df34717cbc70d239f6eeef7b7adcaba82
Flavio-Daniel <flavio.alves@nubank.com.br>
2020-07-18 15:48:48 -0300

    create functions in mock db

--- src/personal_finance/db.clj
+++ src/personal_finance/db.clj
@@ -1,4 +1,31 @@
 (ns personal-finance.db)
 
+(def registries
+  (atom []))
+
+(defn transactions []
+  @registries)
+
 (defn register [transaction]
-  transaction)
+  (let [updated-collection (swap! registries conj transaction)]
+    (merge transaction {:id (count updated-collection)})))
+
+(defn clean []
+  (reset! registries []))
+
+(defn- expense? [transaction]
+  (= (:type transaction) "expense"))
+
+(defn- calculate [acc transaction]
+  (let [value (:value transaction)]
+    (if (expense? transaction)
+      (- acc value)
+      (+ acc value))))
+
+(defn balance []
+  (reduce calculate 0 @registries))
+
+(defn transactions-of-type [type]
+  (filter #(= type (:type %)) (transactions)))
+
+
