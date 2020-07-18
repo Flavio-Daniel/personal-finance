1162995826c7fc8d179bd5aed56d629fbb8757d7
Flavio-Daniel <flavio.alves@nubank.com.br>
2020-07-18 15:49:39 -0300

    create unit tests for handler

--- test/unit/personal_finance/handler_test.clj
+++ test/unit/personal_finance/handler_test.clj
@@ -13,7 +13,10 @@
                (:body response) => "Hello World")))
 
 (facts "Starting balance is zero" :unit
-       (against-background (json/generate-string {:balance 0}) => "{\"balance\":0}")
+       (against-background [(json/generate-string {:balance 0})
+                            => "{\"balance\":0}"
+                            ;;mock para chamada db/balance
+                            (db/balance) => 0])
 
        (let [response (app (mock/request :get "/balance"))]
          (fact "the response status is 200"
@@ -42,5 +45,28 @@
                (:status response) => 201)
          (fact "the body text is a json with the content and an id"
                (:body response) => "{\"id\":1,\"value\":10,\"type\":\"revenue\"}")))
+
+(facts "There is a route to deal with the filter os transaction by type"
+       (against-background
+         [(db/transactions-of-type "revenue") => '({:id 1 :value 2000 :type "revenue"})
+          (db/transactions-of-type "expense") => '({:id 2 :value 89 :type "expense"})
+          (db/transactions) => '({:id 1 :value 2000 :type "revenue"}
+                                 {:id 2 :value 89 :type "expense"})]
+         (fact "Filter by revenue"
+               (let [response (app (mock/request :get "/revenues"))]
+                 (:status response) => 200
+                 (:body response) => (json/generate-string {:transactions '({:id 1 :value 2000 :type "revenue"})})))
+
+         (fact "Filter by expense"
+               (let [response (app (mock/request :get "/expenses"))]
+                 (:status response) => 200
+                 (:body response) => (json/generate-string {:transactions '({:id 2 :value 89 :type "expense"})})))
+
+         (fact "Without transaction filter"
+               (let [response (app (mock/request :get "/transactions"))]
+                 (:status response) => 200
+                 (:body response) => (json/generate-string {:transactions '({:id 1 :value 2000 :type "revenue"}
+                                                                            {:id 2 :value 89 :type "expense"})})))))
+
 
 
